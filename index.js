ad2a9a587f000581c172325452a5171eb5dc89c6
Havard E <havarde.kodehode@gmail.com>
2023-05-25 12:13:18 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://realtime-database-dc353-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const booksInDB = ref(database, "books")
+
+const booksEl = document.getElementById("books")
+
+onValue(booksInDB, function(snapshot){
+    let booksArray = Object.values(snapshot.val())
+    for (let index = 0; index < booksArray.length; index++) {
+        let currentBook = booksArray[index];
+
+        appendBookToBooksListEl(currentBook)
+        
+    }
+   
+})
+
+function clearBooksListEl() {
+    booksEl.innerHTML = ""
+}
+
+function appendBookToBooksListEl(bookValue) {
+    booksEl.innerHTML += `<li>${bookValue}</li>`
+}
